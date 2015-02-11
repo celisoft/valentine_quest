a4a713a4ca9f781c41f0028f4b0ef344e9ae0f9d
Celine Liberal <celine.liberal@mail.com>
2015-02-11 14:39:25 +0100

    Position class

---
+++ position.cpp
@@ -0,0 +1,32 @@
+#include "position.h"
+
+int Position::get_x()
+{
+	return coord_x;
+}
+
+int Position::get_y()
+{
+	return coord_y;
+}
+
+void Position::set_x(int pX=0)
+{
+	if(pX >= MIN_X && pX <= MAX_X)
+	{	
+		coord_x = pX;
+	}
+}
+
+void Position::set_y(int pY=0)
+{
+	if(pY >= MIN_Y && pY <= MAX_Y)
+	{
+		coord_y = pY;
+	}
+}
+
+const int Position::MIN_X;
+const int Position::MAX_X;
+const int Position::MIN_Y;
+const int Position::MAX_Y;
