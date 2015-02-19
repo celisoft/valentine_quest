adbf0f59a54fd7af079a02b8550ef7e12e091cfd
Celine Liberal <celine.liberal@mail.com>
2015-02-19 09:59:53 +0100

    Remove commented code

--- main.cpp
+++ main.cpp
@@ -12,36 +12,6 @@ using namespace std;
 
 int main()
 {
-	//int lvlmap[12][16] {{0}};
-	
-	//ifstream fic("test.lvlmap");
-	//if(fic.is_open())
-	//{
-	//	string line;
-	//	int line_idx = 0;
-	//	int col_idx = 0;
-//
-//		while(getline(fic, line))
-//		{
-//			for(auto lChar : line)
-//			{
-//				if(lChar == '*')
-//				{
-//					lvlmap[line_idx][col_idx] = 1;
-//				}
-//				col_idx++;
-//			}
-//			col_idx = 0;
-//			line_idx++;
-//		}
-//		fic.close();
-//		cout << "Level is loaded !" << endl;
-//	}
-//	else
-//	{
-//		cerr << "Cannot read the level map" << endl;
-//	}
- 
 	GameWindow lWindow;
 	if(lWindow.run() == false)
 	{
