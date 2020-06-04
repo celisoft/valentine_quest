f49e3719c82a7d3a4762a1e6a9568b7433fee2c0
Celine / Celisoft <celine.liberal@mail.com>
2020-06-04 22:15:25 +0200

    cpplint

--- src/door.cpp
+++ src/door.cpp
@@ -1,22 +1,18 @@
 #include "door.hpp"
 
-//Initialize texture
-bool Door::init_texture(SDL_Renderer* pRenderer)
-{
+// Initialize texture
+bool Door::init_texture(SDL_Renderer* pRenderer) {
 	door_texture = SDL_CreateTextureFromSurface(pRenderer, door_image);
-	if(door_texture <= 0)
-	{
+	if(door_texture <= 0) {
 		return false;
 	}
 	SDL_FreeSurface(door_image);
 	return true;
 }
 
-//Render the texture through given renderer
-void Door::render(SDL_Renderer* pRenderer)
-{
-	if(is_open)
-	{
+// Render the texture through given renderer
+void Door::render(SDL_Renderer* pRenderer) {
+	if(is_open) {
 		sprite_rect.x = 128;
 	}
 	SDL_RenderCopy(pRenderer, door_texture, &sprite_rect, &door_rect);
--- src/door.hpp
+++ src/door.hpp
@@ -1,8 +1,8 @@
-#ifndef DOOR_H
-#define DOOR_H
+#ifndef DOOR_HPP_
+#define DOOR_HPP_
 
 #include "position.hpp"
-#include <string>
+
 #include <SDL2/SDL.h>
 
 #ifdef __APPLE__
@@ -11,22 +11,20 @@
 #include <SDL2/SDL_image.h>
 #endif
 
-using namespace std;
+#include <string>
 
-class Door
-{
+class Door {
 	private:
 		Position pos;
-		SDL_Surface* door_image;	
+		SDL_Surface* door_image;
 		SDL_Texture* door_texture;
 		SDL_Rect sprite_rect;
 		SDL_Rect door_rect;
 		bool is_open = false;
 
 	public:
-		//Constructor
-		Door(string pDoorPath="", int pX=0, int pY=0)
-		{
+		// Constructor
+		explicit Door(std::string pDoorPath = "", int pX = 0, int pY = 0) {
 			pos = Position(pX, pY);
 
 			door_image = IMG_Load(pDoorPath.c_str());
@@ -42,23 +40,23 @@ class Door
 			door_rect.y = pos.get_y() * 64;
 		}
 
-		//Open the door
-		void open(){is_open=true;}
+		// Open the door
+		void open() {is_open = true;}
 
-		//Return boolean value indicating if the door is open
-		bool is_opened(){return is_open;}
+		// Return boolean value indicating if the door is open
+		bool is_opened() {return is_open;}
 
-		//Getter for door_rect (will be used for collsion)
-		SDL_Rect* get_rect(){ return &door_rect; }
+		// Getter for door_rect (will be used for collsion)
+		SDL_Rect* get_rect() { return &door_rect; }
 
-		//Render the texture through given renderer
+		// Render the texture through given renderer
 		void render(SDL_Renderer* pRenderer);
 
-		//Initialize texture
+		// Initialize texture
 		bool init_texture(SDL_Renderer* pRenderer);
 
-		//Getter for door texture
-		SDL_Texture* get_texture(){return door_texture;}
+		// Getter for door texture
+		SDL_Texture* get_texture() {return door_texture;}
 };
 
-#endif
+#endif  // DOOR_HPP_
