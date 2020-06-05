c54655a4134bda25aa120784a7f80b1335e8f477
Celine / Celisoft <celine.liberal@mail.com>
2020-06-05 23:19:49 +0200

    cpplint

--- src/position.cpp
+++ src/position.cpp
@@ -1,27 +1,21 @@
 #include "position.hpp"
 
-int Position::get_x()
-{
+int Position::get_x() {
 	return coord_x;
 }
 
-int Position::get_y()
-{
+int Position::get_y() {
 	return coord_y;
 }
 
-void Position::set_x(int pX=0)
-{
-	if(pX >= MIN_X && pX <= MAX_X)
-	{	
+void Position::set_x(int pX = 0) {
+	if(pX >= MIN_X && pX <= MAX_X) {
 		coord_x = pX;
 	}
 }
 
-void Position::set_y(int pY=0)
-{
-	if(pY >= MIN_Y && pY <= MAX_Y)
-	{
+void Position::set_y(int pY = 0) {
+	if(pY >= MIN_Y && pY <= MAX_Y) {
 		coord_y = pY;
 	}
 }
--- src/position.hpp
+++ src/position.hpp
@@ -1,10 +1,9 @@
-#ifndef POSITION_H
-#define POSITION_H
+#ifndef POSITION_HPP_
+#define POSITION_HPP_
 
-class Position
-{
- 	int coord_x;
-      	int coord_y;	
+class Position {
+	int coord_x;
+    int coord_y;
 
 	public:
 		static const int MIN_X = 0;
@@ -12,20 +11,19 @@ class Position
 		static const signed int MIN_Y = 0;
 		static const int MAX_Y = 12;
 
-		//Constructor
-		Position(int pX=0, int pY=0)
-		{
+		// Constructor
+		explicit Position(int pX = 0, int pY = 0) {
 			set_x(pX);
 			set_y(pY);
 		}
 
-		//Getters
+		// Getters
 		int get_x();
 		int get_y();
 
-		//Setters
+		// Setters
 		void set_x(int pX);
 		void set_y(int pY);
 };
 
-#endif
+#endif  // POSITION_HPP_
