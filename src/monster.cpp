14e9ebecb4e7ed182a9a5092f4f2bbc8ef2c24fd
Celine / Celisoft <celine.liberal@mail.com>
2020-06-05 23:42:18 +0200

    cpplint

--- src/monster.cpp
+++ src/monster.cpp
@@ -1,84 +1,63 @@
 #include "monster.hpp"
 
-//Initialize texture
-bool Monster::init_texture(SDL_Renderer* pRenderer)
-{
+// Initialize texture
+bool Monster::init_texture(SDL_Renderer* pRenderer) {
 	monster_texture = SDL_CreateTextureFromSurface(pRenderer, monster_image);
-	if(monster_texture <= 0)
-	{
+	if(monster_texture <= 0) {
 		return false;
 	}
 	SDL_FreeSurface(monster_image);
 	return true;
 }
 
-//Render the texture through given renderer
-void Monster::render(SDL_Renderer* pRenderer)
-{
+// Render the texture through given renderer
+void Monster::render(SDL_Renderer* pRenderer) {
 	SDL_RenderCopy(pRenderer, monster_texture, &sprite_rect, &monster_rect);
 }
 
-//Set player position to the given one
-void Monster::set_pos(Position pPosition)
-{
+// Set player position to the given one
+void Monster::set_pos(Position pPosition) {
 	pos = pPosition;
 
 	monster_rect.x = pPosition.get_x() * STEP_X;
 	monster_rect.y = pPosition.get_y() * STEP_Y;
 }
 
-//Move the monster and refresh its surface
-void Monster::move()
-{
+// Move the monster and refresh its surface
+void Monster::move() {
 	int x_val{0};
 	int y_val{pos.get_y()};
 
-	if(direction == RIGHT)
-	{
-		if(pos.get_x() < x_max)
-		{
+	if(direction == RIGHT) {
+		if(pos.get_x() < x_max) {
 			x_val = pos.get_x() + 1;
 			set_pos(Position(x_val, y_val));
-		}
-		else
-		{
+		} else {
 			direction = LEFT;
 		}
 
-		if(sprite_rect.x == RIGHT_POS_0)
-		{
+		if(sprite_rect.x == RIGHT_POS_0) {
 			sprite_rect.x = RIGHT_POS_1;
-		}
-		else
-		{
+		} else {
 			sprite_rect.x = RIGHT_POS_0;
 		}
-	}
-	else
-	{
-		if(pos.get_x() > x_min)
-		{
+	} else {
+		if(pos.get_x() > x_min) {
 			x_val = pos.get_x() - 1;
 			set_pos(Position(x_val, y_val));
-		}
-		else
-		{
+		} else {
 			direction = RIGHT;
 		}
 
-		if(sprite_rect.x == LEFT_POS_0)
-		{
+		if(sprite_rect.x == LEFT_POS_0) {
 			sprite_rect.x = LEFT_POS_1;
-		}
-		else
-		{
+		} else {
 			sprite_rect.x = LEFT_POS_0;
 		}
 	}
 }
 
-//Destroy
-void Monster::dispose()
-{
-	SDL_DestroyTexture(monster_texture);	
+// Destroy
+void Monster::dispose() {
+	SDL_DestroyTexture(monster_texture);
 }
--- src/monster.hpp
+++ src/monster.hpp
@@ -1,7 +1,10 @@
-#ifndef MONSTER_H
-#define MONSTER_H
+#ifndef MONSTER_HPP_
+#define MONSTER_HPP_
 
 #include "position.hpp"
+
+#include <string>
+
 #include <SDL2/SDL.h>
 #ifdef __APPLE__
 #include <SDL2_image/SDL_image.h>
@@ -9,13 +12,10 @@
 #include <SDL2/SDL_image.h>
 #endif
 
-#include <string>
-
-class Monster
-{
+class Monster {
 	private:
 		Position pos;
-		SDL_Surface* monster_image;	
+		SDL_Surface* monster_image;
 		SDL_Texture* monster_texture;
 		SDL_Rect sprite_rect;
 		SDL_Rect monster_rect;
@@ -24,11 +24,11 @@ class Monster
 		int x_min;
 		int x_max;
 
-		//Move on X axis 
+		// Move on X axis
 		void move_x(int step);
 
 	public:
-		//Constant initialization
+		// Constant initialization
 		static const int RIGHT = 0;
 		static const int LEFT = 1;
 		static const int LEFT_POS_0 = 3*64;
@@ -38,9 +38,8 @@ class Monster
 		static const int STEP_X = 64;
 		static const int STEP_Y = 64;
 
-		//Constructor
-		Monster(std::string pPath, int pXmin, int pXmax, int pY)
-		{
+		// Constructor
+		Monster(std::string pPath, int pXmin, int pXmax, int pY) {
 			pos = Position(pXmin, pY);
 
 			direction = RIGHT;
@@ -60,24 +59,23 @@ class Monster
 			monster_rect.y = pY * 64;
 		}
 
-		//Getter for monster_rect (will be used for collsion)
-		SDL_Rect* get_rect(){ return &monster_rect; }
+		// Getter for monster_rect (will be used for collsion)
+		SDL_Rect* get_rect() { return &monster_rect; }
 
-		//Initialize the monster texture
+		// Initialize the monster texture
 		bool init_texture(SDL_Renderer* pRenderer);
 
-		//Destroy
+		// Destroy
 		void dispose();
 
-		//Set the player position to the given position
+		// Set the player position to the given position
 		void set_pos(Position pPosition);
-		
-		//Move
-		void move();
-		
-		//Render the texture through given renderer
-		void render(SDL_Renderer* pRenderer);
 
+		// Move
+		void move();
+
+		// Render the texture through given renderer
+		void render(SDL_Renderer* pRenderer);
 };
 
-#endif
+#endif  // MONSTER_HPP_
